2dd68fca0170f4f84bfdcb9dd81f61ceb7b1b60a
枝藻 花 <1729438603@qq.com>
2025-07-04 09:18:10 +0800

    WPF计算器

---
+++ Calculator/CalculatorMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal static class CalculatorMethods
+    {
+        //计算
+        public static double Add(double x, double y)
+            { return x + y; }
+        public static double Sub(double x, double y)
+            { return x - y; }
+        public static double Mul(double x, double y)
+            { return x * y; }
+        public static double Div(double x, double y)
+            { return x / y; }
+    }
+}
